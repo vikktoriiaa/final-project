eaa0c7ec584902edb783752471665db3f910dcec
viktoria mojseenok <viktoriamojseenok@gmail.com>
2025-08-09 22:21:15 +0300

    add Main

---
+++ vite-project-40/src/components/Main.tsx
@@ -0,0 +1,15 @@
+import { THEME } from "../constants/theme"
+import { FormController } from "./form/FormController"
+
+type Props = {
+  theme: string,
+  onBackToHome: () => void
+}
+export const Main = ({theme, onBackToHome}: Props) => {
+  return (
+    <main className={`flex-1 flex flex-col gap-4 ${theme === THEME.DARK ? 'text-white bg-[#313037]' : 'text-black bg-[#f3f3f3]'}`}>
+      <button className="border-1 border-gray-300 mt-10 mb-5 mx-auto p-3 rounded-xl" onClick={onBackToHome}>Back to home</button>
+      <FormController />
+    </main>
+  )
+}
