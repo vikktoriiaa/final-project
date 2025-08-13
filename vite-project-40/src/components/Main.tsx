1ebc58b810ddd3839ed37429c6852e4c849e5c1f
viktoria mojseenok <viktoriamojseenok@gmail.com>
2025-08-13 23:15:23 +0300

    change Main.tsx

--- vite-project-40/src/components/Main.tsx
+++ vite-project-40/src/components/Main.tsx
@@ -1,6 +1,6 @@
 import { THEME } from "../constants/theme"
 import { FormController } from "./form/FormController"
-import { FormWithFocus } from "./FormWithFocus"
+// import { FormWithFocus } from "./FormWithFocus"
 
 type Props = {
   theme: string,
@@ -11,7 +11,7 @@ export const Main = ({theme, onBackToHome}: Props) => {
     <main className={`flex-1 flex flex-col gap-4 ${theme === THEME.DARK ? 'text-white bg-[#313037]' : 'text-black bg-[#f3f3f3]'}`}>
       <button className="border-1 border-gray-300 mt-10 mb-5 mx-auto p-3 rounded-xl" onClick={onBackToHome}>Back to home</button>
       <FormController />
-      <FormWithFocus />
+      {/* <FormWithFocus /> */}
     </main>
   )
 }
