4b31e3ce958bedc99788eb3c69e18ee13bae948c
viktoria mojseenok <viktoriamojseenok@gmail.com>
2025-08-13 19:01:50 +0300

    change FormController.tsx

--- vite-project-40/src/components/form/FormController.tsx
+++ vite-project-40/src/components/form/FormController.tsx
@@ -1,5 +1,18 @@
-import { FormView } from "./FormView"
-
+import { InputWithLabel } from "./FormView"
+import { useState } from "react"
 export const FormController = () => {
-  return <FormView />
+  const [formData, setFormData] = useState({
+    name:'',
+    email: '',
+    password: '',
+  })
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const key = e.target.name
+    console.log(formData)
+    setFormData({ ...formData, [key]: e.target.value })
+    
+  }
+  return <InputWithLabel name={formData.name} email={formData.email} password={formData.password} onChange={handleOnChange}/>
 }
+ 
