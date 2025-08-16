947159c201bd45762e10c3f0d38a9f271064e134
viktoria mojseenok <viktoriamojseenok@gmail.com>
2025-08-17 00:22:51 +0300

    change Header

--- vite-project-40/src/components/Header.tsx
+++ vite-project-40/src/components/Header.tsx
@@ -1,20 +1,32 @@
+import { useTheme } from "@/hooks/useTheme";
+import { SearchView } from "./search/SearchView";
+
 type Props = {
-  onChangeTheme: () => void
-  onSignIn: () => void,
-}
-export const Header = ({onChangeTheme, onSignIn}: Props) => {
+  onSignIn: () => void;
+};
+export const Header = ({ onSignIn }: Props) => {
+  const { toggleTheme } = useTheme();
+
   return (
     <header className="w-full h-auto bg-[#2436a7] flex justify-between items-center-center p-5">
-      <button className='bg-blue-400 text-white p-2 rounded-2xl'>
+      <button className="bg-blue-400 text-white p-2 rounded-2xl">
         <img src="../public/burger.svg" alt="burger-menu" className="w-8 h-8" />
       </button>
-      <div className = 'flex gap-1'>
-        <button className='bg-blue-400 rounded-2xl text-white p-2'>
-          <img src="../public/search.svg" alt="search" className="w-6 h-6" />
+      <div className="flex gap-1">
+        <SearchView />
+        <button
+          className="bg-blue-400 rounded-2xl text-white p-2"
+          onClick={onSignIn}
+        >
+          Sign In
         </button>
-        <button className='bg-blue-400 rounded-2xl text-white p-2' onClick={onSignIn}>Sign In</button>
-        <button className='bg-blue-400 rounded-2xl text-white p-2' onClick={onChangeTheme}>Change theme</button>
+        <button
+          className="bg-blue-400 rounded-2xl text-white p-2"
+          onClick={toggleTheme}
+        >
+          Change theme
+        </button>
       </div>
     </header>
-  )
-}
+  );
+};
