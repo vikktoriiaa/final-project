1b86e601922f88234f9a14651c3eef70e8c13737
viktoria mojseenok <viktoriamojseenok@gmail.com>
2025-08-17 00:21:24 +0300

    change App.tsx

--- vite-project-40/src/App.tsx
+++ vite-project-40/src/App.tsx
@@ -1,30 +1,36 @@
-import { Header } from './components/Header'
-import { Main } from './components/Main'  
-import { Footer } from './components/Footer'
-import { useState } from 'react'
-import { THEME } from './constants/theme'
-import { ListController } from './components/CardsPosts/ListCardsPosts/ListController'
+import { Header } from "./components/Header";
+import { Main } from "./components/Main";
+import { Footer } from "./components/Footer";
+import { useState } from "react";
+import { THEME } from "./constants/theme";
+import { ListController } from "./components/CardsPosts/ListCardsPosts/ListController";
+import { useTheme } from "./hooks/useTheme";
+import { PostProvider } from "./contexts/PostContexts";
 
 export function App() {
+  const { theme } = useTheme();
 
-  const [theme, setTheme] = useState(THEME.LIGHT)
-  const [page, setPage] = useState<'home'|'form'>('form');
+  const [page, setPage] = useState<"home" | "form">("home");
 
-  const changeTheme = () => {
-    setTheme(theme === THEME.LIGHT ? THEME.DARK : THEME.LIGHT)
-  }
   return (
-    <div className='flex flex-col min-h-screen'>
-      {page === 'home' && <Header onChangeTheme={changeTheme} onSignIn={() => setPage('form')} />}
-      {page === 'form' && <Header onChangeTheme={changeTheme} onSignIn={() => setPage('form')} />}
-      {page === 'home' &&
-        <div className={`${theme === THEME.DARK ? 'text-white bg-[#313037]' : 'text-black bg-[#f3f3f3]'}`}>
-          <ListController theme={theme} />
-        </div>}
-      {page === 'form' && <Main theme={theme} onBackToHome={() => setPage('home')} />}
-      <Footer theme={theme} />
-    </div>
-  )
+    <PostProvider>
+      <div className="flex flex-col min-h-screen">
+        {page === "home" && <Header onSignIn={() => setPage("form")} />}
+        {page === "form" && <Header onSignIn={() => setPage("form")} />}
+        {page === "home" && (
+          <div
+            className={`${
+              theme === THEME.DARK
+                ? "text-white bg-[#313037]"
+                : "text-black bg-[#f3f3f3]"
+            }`}
+          >
+            <ListController />
+          </div>
+        )}
+        {page === "form" && <Main onBackToHome={() => setPage("home")} />}
+        <Footer />
+      </div>
+    </PostProvider>
+  );
 }
-
-
