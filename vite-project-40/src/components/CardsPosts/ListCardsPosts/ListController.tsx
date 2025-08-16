795d7c44c8ae4f90fa07ef4fbec2771688163a0f
viktoria mojseenok <viktoriamojseenok@gmail.com>
2025-08-17 00:24:51 +0300

    change ListCardsPosts

--- vite-project-40/src/components/CardsPosts/ListCardsPosts/ListController.tsx
+++ vite-project-40/src/components/CardsPosts/ListCardsPosts/ListController.tsx
@@ -1,9 +1,5 @@
 import { ListView } from "./ListView"
 
-type Props = {
-  theme: string
-}
-
-export const ListController = ({theme}: Props) => {
-  return <ListView theme={theme } />
+export const ListController = () => {
+  return <ListView/>
 }
