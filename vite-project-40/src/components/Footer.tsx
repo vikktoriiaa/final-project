a6e19e210709e4640c6276e8d3b7b5b19aa9dc87
viktoria mojseenok <viktoriamojseenok@gmail.com>
2025-08-17 00:23:04 +0300

    change Footer

--- vite-project-40/src/components/Footer.tsx
+++ vite-project-40/src/components/Footer.tsx
@@ -1,9 +1,8 @@
+import { useTheme } from "@/hooks/useTheme"
 import { THEME } from "../constants/theme"
 
-type Props = {
-  theme: string
-}
-export const Footer = ({theme}: Props) => {
+export const Footer = () => {
+  const {theme} = useTheme()
   return (
    <footer className={`w-full flex justify-between border-t-gray-300 border-t-solid border-t-1 p-5 ${theme === THEME.DARK ? 'text-white bg-[#313037]' : 'text-black bg-[#f3f3f3]'}`}>
     <span className="copyright text-gray-400">@{new Date().getFullYear()} copyright </span>
