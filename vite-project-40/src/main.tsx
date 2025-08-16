9907e08a17f0d4bd2b94722d8205725cc134ade0
viktoria mojseenok <viktoriamojseenok@gmail.com>
2025-08-17 00:20:52 +0300

    add Main.tsx

--- vite-project-40/src/main.tsx
+++ vite-project-40/src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { App } from './App.tsx'
+import { ThemeProvider } from './contexts/ThemeContexts.tsx'
+import { PostProvider } from './contexts/PostContexts.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <PostProvider>
+        <App />
+      </PostProvider>
+    </ThemeProvider>
   </StrictMode>,
 )
