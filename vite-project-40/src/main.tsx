4cffa108915168c4e5b342bf70a8600c886edf49
viktoria mojseenok <viktoriamojseenok@gmail.com>
2025-08-17 21:34:25 +0300

    change main.tsx

--- vite-project-40/src/main.tsx
+++ vite-project-40/src/main.tsx
@@ -1,16 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { App } from './App.tsx'
-import { ThemeProvider } from './contexts/ThemeContexts.tsx'
-import { PostProvider } from './contexts/PostContexts.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { App } from "./App.tsx";
+import { ThemeProvider } from "./contexts/ThemeContexts.tsx";
+import { PostProvider } from "./contexts/PostContexts.tsx";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
       <PostProvider>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </PostProvider>
     </ThemeProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
