f803142e00fc90bba2d29c3842bff426bacf837b
viktoria mojseenok <viktoriamojseenok@gmail.com>
2025-08-19 22:41:08 +0300

    add changes

--- final-project/src/components/Main.tsx
+++ final-project/src/components/Main.tsx
@@ -1,5 +1,8 @@
 import type { FC } from "react"
 import { Button } from "./common/Button"
+import { Author } from "./common/Author"
+import { Post } from "./common/Post"
+import { TitleSection } from "./common/TitleSection"
 
 export const Main:FC = () => {
   return (
@@ -9,14 +12,28 @@ export const Main:FC = () => {
         <div className="relative py-40 pl-20 pr-120 text-white">
           <p className="uppercase text-base/5 font-medium tracking-[3px] pb-6">Posted on <span className="font-bold">startup</span></p>
           <h1 className="text-[56px]/16 font-bold pb-6 -tracking-[2px]">Step-by-step guide to choosing great font pairs</h1>
-          <p className="font-normal text-base/7 pb-4">By <span className="text-yellow-300">James West</span> |  May 23, 2022 </p>
+          <p className="font-normal text-base/7 pb-4">By <span className="text-[#FFD050]">James West</span> |  May 23, 2022 </p>
           <p className="font-normal text-base/7 pr-80 pb-12">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.</p>
           <Button text="Read More >" background="bg-[#FFD050]"/>
         </div>
       </section>
-      <section className="relative">
+      <section className="flex py-32 px-20">
         <div>
-          <h1>kadbc</h1>
+          <TitleSection title="Featured Post" />
+          <div>
+            <img src="../../public/feature.png"></img>
+            <Author date="May 23, 2022" />
+            <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</h4>
+            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.</p>
+            <Button text="Read More >" background="bg-[#FFD050]"/>
+          </div>
+        </div>
+        <div>
+          <TitleSection title={"All Posts"} />
+          <Post title="8 Figma design systems that you can download for free today." date="Aug 23, 2021" />
+          <Post title="8 Figma design systems that you can download for free today." date="Aug 23, 2021" bg={"bg-[#FBF6EA]"} />
+          <Post title="8 Figma design systems that you can download for free today." date="Aug 23, 2021" />
+          <Post title="8 Figma design systems that you can download for free today." date="Aug 23, 2021" />
         </div>
       </section>
     </main>
