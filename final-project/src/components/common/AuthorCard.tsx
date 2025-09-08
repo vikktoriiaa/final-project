5d136f67ad4593ef01f9c6a44d251c13643660ad
viktoria mojseenok <viktoriamojseenok@gmail.com>
2025-09-08 22:24:30 +0300

    add flip effect

--- final-project/src/components/common/AuthorCard.tsx
+++ final-project/src/components/common/AuthorCard.tsx
@@ -1,4 +1,4 @@
-import type { FC } from "react"
+import { type FC, useState } from "react";
 
 type Props = {
   background: string,
@@ -8,17 +8,28 @@ type Props = {
   facebook: string,
   twitter: string,
   instagram: string,
-  linkedin: string
+  linkedin: string,
+  about: string,
 }
 
-export const AuthorCard:FC<Props> = ({background, icon, text, title, facebook, twitter, instagram, linkedin}) => {
+export const AuthorCard: FC<Props> = ({ background, icon, text, title, facebook, twitter, instagram, linkedin, about }) => {
+  const [flipped, setFlipped] = useState(false);
   return (
-     <div className={`flex flex-col gap-2 ${ background } p-8 items-center justify-center`}>
-      <img src={`${icon}`} alt='icon' className="sm:size-32 xxs:size-24 p-3"></img>
-      <h4 className="font-bold text-2xl/10 text-[#232536]">{ title }</h4>
-      <p className="text-[#6D6E76] font-normal text-sm/5">{ text }</p>
-      <div className="flex gap-4">
-        <button className="cursor-pointer">
+    <div
+      className="relative w-[260px] h-[300px]"
+      onMouseEnter={() => setFlipped(true)}
+      onMouseLeave={() => setFlipped(false)}
+      style={{ perspective: 1000 }}
+    >
+      <div className={`transition-transform duration-500 w-full h-full ${flipped ? 'rotate-y-180' : ''}`}
+           style={{ transformStyle: "preserve-3d" }}>
+        {/* Front Side */}
+        <div className={`absolute backface-hidden flex flex-col gap-2 ${background} p-8 items-center justify-center`}>
+          <img src={icon} alt='icon' className="sm:size-32 xxs:size-24 p-3"/>
+          <h4 className="font-bold text-2xl/10 text-[#232536]">{title}</h4>
+          <p className="text-[#6D6E76] font-normal text-sm/5">{text}</p>
+          <div className="flex gap-4">
+            <button className="cursor-pointer">
           <img src={`${facebook}`} alt='facebook' className="size-4"></img>
         </button>
         <button className="cursor-pointer">
@@ -30,7 +41,14 @@ export const AuthorCard:FC<Props> = ({background, icon, text, title, facebook, t
         <button className="cursor-pointer">
           <img src={`${linkedin}`} alt='linkedin' className="size-4"></img>
         </button>
+          </div>
+        </div>
+        {/* Back Side */}
+        <div className="absolute top-0 left-0 w-[250px] h-[300px] flex items-center justify-center backface-hidden bg-[#F4F4F4] p-4 rotate-y-180">
+          <p className="text-black text-base">{about}</p>
+        </div>
       </div>
     </div>
   )
 }
+
